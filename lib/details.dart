6da57ab00473014e1989f733790423a8f173e43c
MASKOUL AYOUB <100036987+ayoub-maskoul@users.noreply.github.com>
2023-06-08 16:15:53 +0100

    Update details

--- lib/details.dart
+++ lib/details.dart
@@ -1,14 +1,19 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 
 
 class Details extends StatelessWidget {
-    Details({Key? key, required this.name ,required this.address}) :super(key: key);
+    Details({Key? key, required this.name ,required this.address, required this.list}) :super(key: key);
 
   String name;
   String address;
+  Map list;
   @override
   Widget build(BuildContext context) {
+
+ 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Detalis Page"),
@@ -19,28 +24,38 @@ class Details extends StatelessWidget {
           },
           ),
       ),
-      body:  Center(
-        child: Card(
-        margin: const EdgeInsets.all(8),
-        child:  SizedBox(
-          width: 300,
-          height: 100,
-
-          child: Center(
+      body:
+            ListView(
+              children: List.generate(list.length, 
+              (index) {
+                return Card(
+                  margin: const EdgeInsets.all(8.0),
+                  
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text("Name:",style: TextStyle(
+                        fontSize: 20
+                      )),
+                      Text(list.values.toList(growable: false)[index].toString(),style: const TextStyle(
+                        fontSize: 20
+                      )),
+                      const Text("Address:",style: TextStyle(
+                        fontSize: 20
+                      )),
+                      Text(list.keys.toList(growable: false)[index].toString(),style: const TextStyle(
+                        fontSize: 20
+                      )),
+                    ],
+                  )
+          );
+              }
+              ),
+            )
             
-            child: Column(
-            children: [
-            const Text("Name"),
-            Text(name.toString()),
-            const Text("Addresss"),
-            Text(address.toString()),
-        ]
-            ),
-        ),
-
-      ),
-      ),
-      )
+          
     );
   }
+  
+
 }
